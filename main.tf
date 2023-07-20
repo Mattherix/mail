6ff3813ea39e260c558aaa2b31455a2e6df74078
Mattherix <mattherix@protonmail.com>
2023-07-20 21:40:42 +0200

    feat(terraform): step 2 setup server

--- main.tf
+++ main.tf
@@ -27,3 +27,46 @@ resource "local_sensitive_file" "pem_file" {
   directory_permission = "700"
   content              = tls_private_key.remote_management.private_key_openssh
 }
+
+# Step 2: Setup server
+resource "hcloud_primary_ip" "mail_ipv4" {
+  name          = "mail_ipv4"
+  datacenter    = "fsn1-dc14"
+  type          = "ipv4"
+  assignee_type = "server"
+  auto_delete   = true
+  labels = {
+    "mail" : ""
+  }
+}
+
+resource "hcloud_primary_ip" "mail_ipv6" {
+  name          = "mail_ipv6"
+  datacenter    = "fsn1-dc14"
+  type          = "ipv6"
+  assignee_type = "server"
+  auto_delete   = true
+  labels = {
+    "mail" : ""
+  }
+}
+
+resource "hcloud_server" "mail" {
+  name        = "mail"
+  image       = "debian-12"
+  server_type = "cax11"
+  datacenter  = "fsn1-dc14"
+  ssh_keys    = [ hcloud_ssh_key.remote_management.id ]
+  backups     = true
+
+  public_net {
+    ipv4_enabled = true
+    ipv4 = hcloud_primary_ip.mail_ipv4.id
+    ipv6_enabled = true
+    ipv6 = hcloud_primary_ip.mail_ipv6.id
+  }
+
+  labels = {
+    "mail" = ""
+  }
+}
